21b9de314909600744100f51c03847859e0f0ced
Dmitriy Nesteryuk <nesterukd@gmail.com>
2013-04-25 08:31:12 +0300

    added Adjuster class

---
+++ lib/site_prism_vcr/adjuster.rb
@@ -0,0 +1,38 @@
+module SitePrism
+  module Vcr
+    class Adjuster
+      attr_reader :fixtures
+
+      def initialize(fixtures_handler, options = {})
+        @fixtures_handler, @fixtures = fixtures_handler, []
+        @action, @waiter = :replace, options[:waiter]
+      end
+
+      def path(path, fixture_names)
+        fixture_names.map do |name|
+          @fixtures << "#{path}/#{name}"
+        end
+      end
+
+      def waiter(val = nil)
+        if val.nil?
+          @waiter
+        else
+          @waiter = val
+        end
+      end
+
+      def replace
+        @action = :replace
+      end
+
+      def union
+        @action = :union
+      end
+
+      def modify_fixtures
+        @fixtures_handler.apply(fixtures, @action)
+      end
+    end
+  end
+end
