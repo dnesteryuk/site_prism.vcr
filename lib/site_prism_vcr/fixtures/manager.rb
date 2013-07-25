7fd52c69957723125d1afe1eed3cbffa65d3a0bb
Dmitriy Nesteryuk <nesterukd@gmail.com>
2013-07-25 09:53:11 +0300

    adds manager class for inserting and ejecting fixtures from Vcr

---
+++ lib/site_prism_vcr/fixtures/manager.rb
@@ -0,0 +1,38 @@
+module SPV
+  class Fixtures
+    # Takes cares about inserting and ejecting fixtures
+    # from Vcr.
+    class Manager
+      def initialize(options)
+        @options = options
+      end
+
+      # Injects given fixtures for Vcr.
+      #
+      # @param fixtures [Array] List of fixtures.
+      #
+      # @return [void]
+      #
+      # @raise [ArgumentError] If a list with fixtures is empty.
+      def inject(fixtures)
+        raise ArgumentError.new(
+          'No fixtures were specified to insert them into VCR'
+        ) if fixtures.size == 0
+
+        fixtures.each do |fixture|
+          VCR.insert_cassette fixture.name
+        end
+      end
+
+      # Ejects all fixtures from Vcr.
+      # Now it doesn't care which fixtures were inserted by
+      # an instance of this class. After calling this method
+      # Vcr will have no inserted fixtures at all.
+      #
+      # @return [void]
+      def eject
+        SPV::Helpers.eject_all_cassettes
+      end
+    end
+  end
+end
