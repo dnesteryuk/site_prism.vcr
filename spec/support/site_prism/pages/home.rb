45d6c9ba95b3e99acf29b264ac6af071c85d59b7
Dmitriy Nesteryuk <dmitriy.nesteryuk@ecommerce.com>
2015-03-02 16:19:07 +0200

    removed needless code which was used for the previous testing approach

--- spec/support/site_prism/pages/home.rb
+++ spec/support/site_prism/pages/home.rb
@@ -13,15 +13,6 @@ class HomePage < BasePage
     end
 
   element_with_vcr \
-    :link_with_home_path,
-    '#link_with_one_request' do
-      home_path 'custom'
-
-      fixtures ['~/daenerys_targaryen']
-      waiter   &:wait_for_cat_owner
-    end
-
-  element_with_vcr \
     :link_with_one_request_and_delay,
     '#link_with_one_request_and_delay' do
       fixtures ['ned_stark']
@@ -33,13 +24,6 @@ class HomePage < BasePage
     '#link_with_2_requests' do
       fixtures ['ned_stark', 'robb_stark']
       waiter   &:wait_for_ned_stark_and_robb_stark
-    end
-
-  element_with_vcr \
-    :link_without_ejecting_fixtures,
-    '#link_with_2_requests' do
-      fixtures ['ned_stark', 'robb_stark']
-      waiter({eject_cassettes: false}, &:wait_for_ned_stark_and_robb_stark)
     end
 
   element :link_without_vcr, '#link_with_one_request'
